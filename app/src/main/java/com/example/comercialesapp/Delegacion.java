539a396ce64b6124220e7ad9a3ec5f3bf9bef3ab
jonberge1337 <jonbergerandi@gmail.com>
2019-12-12 10:13:16 +0100

    Añadido el proyecto

---
+++ app/src/main/java/com/example/comercialesapp/Delegacion.java
@@ -0,0 +1,34 @@
+package com.example.comercialesapp;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import androidx.fragment.app.FragmentActivity;
+
+public class Delegacion extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delegacion);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(43.3045627,-2.0191253),15.0f));
+
+
+    }
+}
