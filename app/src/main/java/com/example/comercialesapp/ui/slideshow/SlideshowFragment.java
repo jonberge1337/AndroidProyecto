755364487d2c649891227a94e98811659dcc03dd
jonberge1337 <jonbergerandi@gmail.com>
2020-01-07 21:25:51 +0100

    Hemos quitado tabhost de partners

--- app/src/main/java/com/example/comercialesapp/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/comercialesapp/ui/slideshow/SlideshowFragment.java
@@ -28,21 +28,6 @@ public class SlideshowFragment extends Fragment {
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
-        Resources res = getResources();
-        TabHost tabs = root.findViewById(R.id.TabHost);
-        tabs.setup();
-
-        TabHost.TabSpec spec = tabs.newTabSpec("mitab1");
-        spec.setContent(R.id.ACTUALES);
-        spec.setIndicator("TAB1", res.getDrawable(android.R.drawable.ic_btn_speak_now));
-        tabs.addTab(spec);
-
-        spec = tabs.newTabSpec("mitab2");
-        spec.setContent(R.id.NUEVO);
-        spec.setIndicator("TAB2", res.getDrawable(android.R.drawable.ic_dialog_map));
-        tabs.addTab(spec);
-
-
         generarXml();
 
         return root;
