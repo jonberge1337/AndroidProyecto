92458b67437f4868e4687cfade4ae4c6ed2c910b
Adminportatil <adminportatil@POB-DE-3568-20.cebancdea.edu>
2020-01-08 21:16:26 +0100

    Hemos puesto el logo en la pantalla principal

--- app/src/main/java/com/example/comercialesapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/comercialesapp/ui/home/HomeFragment.java
@@ -4,15 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.comercialesapp.R;
 
@@ -21,7 +16,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+
+        TextView titulo = root.findViewById(R.id.lblTitulo_home);
 
         return root;
     }
