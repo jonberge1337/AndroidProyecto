cf3ebc170e28d5356c457bc112de5ab317b0e67f
Adminportatil <adminportatil@POB-DE-3568-20.cebancdea.edu>
2020-01-11 22:34:31 +0100

    Metido el mapa sin saber si funciona

--- app/src/main/java/com/example/comercialesapp/ui/delegacion/GalleryFragment.java
+++ app/src/main/java/com/example/comercialesapp/ui/delegacion/GalleryFragment.java
@@ -13,23 +13,36 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.comercialesapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnMapReadyCallback {
 
     private GalleryViewModel galleryViewModel;
+    private GoogleMap mapa;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_delegacion, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mapa = googleMap;
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.3045627,-2.0191253),15.0f));
+
     }
 }
