808d8ef06a5a848170382cb23bf2f9400c6d1bd0
jonberge1337 <jonbergerandi@gmail.com>
2020-01-12 00:53:17 +0100

    hecho listview pedidos layout

--- app/src/main/java/com/example/comercialesapp/ui/pedidos/Catalogo.java
+++ app/src/main/java/com/example/comercialesapp/ui/pedidos/Catalogo.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.comercialesapp.R;
@@ -22,7 +19,7 @@ public class Catalogo extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         toolsViewModel =
                 ViewModelProviders.of(this).get(ToolsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_pedidos, container, false);
+        View root = inflater.inflate(R.layout.fragment_articulo, container, false);
 
 
 
