c768986d026413436ca860c5d51d079100fd8585
jonberge1337 <jonbergerandi@gmail.com>
2020-01-31 23:53:30 +0100

    ya terminando falta probar y ultimos retoques

--- app/src/main/java/com/example/comercialesapp/ui/pedidos/ResumenPedido.java
+++ app/src/main/java/com/example/comercialesapp/ui/pedidos/ResumenPedido.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +23,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class ResumenPedido extends Fragment implements View.OnClickListener {
-    private ImageButton botonInicio;
+    private ImageButton botonGuardar;
     private ArrayList<Pedido> pedidos = new ArrayList<>();
     private ArrayList<String> partners = new ArrayList<>();
     private ListView listaPartners;
@@ -72,6 +71,9 @@ public class ResumenPedido extends Fragment implements View.OnClickListener {
             partners.add(c.getString(c.getColumnIndex("EMPRESA")));
         }
 
+        c.close();
+        db.close();
+
         ArrayAdapter<String> adaptadorPartners = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, partners);
         partner = root.findViewById(R.id.spinnerPartner);
         partner.setAdapter(adaptadorPartners);
@@ -80,14 +82,46 @@ public class ResumenPedido extends Fragment implements View.OnClickListener {
         listaPartners = root.findViewById(R.id.lstResumenPedido);
         listaPartners.setAdapter(adaptador);
 
-        this.botonInicio = root.findViewById(R.id.btnResumenPedidoHome);
-        this.botonInicio.setOnClickListener(this);
+        this.botonGuardar = root.findViewById(R.id.btnResumenPedidoHome);
+        this.botonGuardar.setOnClickListener(this);
         return root;
     }
 
 
     @Override
     public void onClick(View v) {
+        int partnerid = partner.getSelectedItemPosition();
+        int comercialID = 0;
+        int pedidoID;
+        String consulta;
+
+        TablaSQL tabla = new TablaSQL(getContext(), "DBUsuarios", null, 1);
+        final SQLiteDatabase db = tabla.getWritableDatabase();
+
+        Cursor c;
+        consulta = "SELECT * FROM COMERCIAL WHERE LOGUEADO = 1";
+        c = db.rawQuery(consulta, null);
+        if (c.moveToFirst()){
+            comercialID = c.getInt(c.getColumnIndex("COMERCIALID"));
+        }
+
+        consulta = "INSERT INTO CAB_PEDIDO(FECHAPEDIDO, COMERCIALID, PARTNERID)" +
+                "VALUES(date('now'), "+ comercialID + "," + partnerid + ")";
+        db.execSQL(consulta);
+
+        consulta = "SELECT last_insert_rowid() AS ULTIMO";
+        c = db.rawQuery(consulta, null);
+        pedidoID = c.getInt(c.getColumnIndex("ULTIMO"));
+
+        for (Pedido pedido : pedidos){
+            consulta = "INSERT INTO LINEA VALUES(" + pedidoID + ", " + pedido.getArticuloID() +
+                    ", " + pedido.getPrecio() + ", " + pedido.getCantidad() + ", " + pedido.getDescuento() + ")";
+            db.execSQL(consulta);
+        }
+
+        c.close();
+        db.close();
+
         Intent intencion = new Intent(Objects.requireNonNull(getActivity()).getApplicationContext(), MainActivity.class);
         startActivity(intencion);
     }
